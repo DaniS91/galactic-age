c3b0accd1df2955c1fa59a65fef6f83f32ef99d9
DaniS91 <114887061+DaniS91@users.noreply.github.com>
2022-12-21 19:31:29 -0800

    add clean webpack plugin config to webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -8,8 +9,9 @@ module.exports = {
     path: path.resolve(__dirname, 'dist')
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Shape Tracker',
+      title: 'Galactic Age Calculator',
       template: './src/index.html',
       inject: 'body'
     })
