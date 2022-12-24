ab56ea541b6e5f93ac60c789d8e8172c70771c91
DaniS91 <114887061+DaniS91@users.noreply.github.com>
2022-12-23 21:31:28 -0800

    TDD red phase: setup test for sinceMercuryAge property in class constructor

--- __tests__/age-calc.test.js
+++ __tests__/age-calc.test.js
@@ -31,4 +31,9 @@ describe('GalacticAge', () => {
     const myGalacticAge = new GalacticAge(31, 21);
     expect(myGalacticAge.sinceEarthAge).toEqual(31 - 21);
   });
+
+  test('should correctly create a sinceMercuryAge property in GalacticAge class', () => {
+    const myGalacticAge = new GalacticAge(31, 21);
+    expect(myGalacticAge.sinceMercuryAge).toEqual((31 - 21) * .24);
+  });
 });
