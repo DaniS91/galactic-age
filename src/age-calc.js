b2902cfbb44588c5f94b53363188539ef0d1cca8
DaniS91 <114887061+DaniS91@users.noreply.github.com>
2022-12-23 21:27:07 -0800

    TDD green phase: test passed for sinceEarthAge property in class constructor

--- src/age-calc.js
+++ src/age-calc.js
@@ -1,9 +1,10 @@
 export default class GalacticAgeCalc {
-  constructor(age) {
+  constructor(age, passedAge) {
     this.earthAge = age;
     this.mercuryAge = age * .24;
     this.venusAge = age * .62;
     this.marsAge = age * 1.88;
     this.jupiterAge = age * 11.86;
+    this.sinceEarthAge = age - passedAge;
   }
 }
