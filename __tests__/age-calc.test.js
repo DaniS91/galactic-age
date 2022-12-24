5cfad61b91db4b44531b74cf3fba4d0b7e6261d2
DaniS91 <114887061+DaniS91@users.noreply.github.com>
2022-12-23 21:22:56 -0800

    fix naming mistake in the test 'should' statements

--- __tests__/age-calc.test.js
+++ __tests__/age-calc.test.js
@@ -12,17 +12,17 @@ describe('GalacticAge', () => {
     expect(myGalacticAge.mercuryAge).toEqual(31 * .24);
   });
 
-  test('should correctly create a mercuryAge property in GalacticAge class', () => {
+  test('should correctly create a venusAge property in GalacticAge class', () => {
     const myGalacticAge = new GalacticAge(31);
     expect(myGalacticAge.venusAge).toEqual(31 * .62);
   });
 
-  test('should correctly create a mercuryAge property in GalacticAge class', () => {
+  test('should correctly create a marsAge property in GalacticAge class', () => {
     const myGalacticAge = new GalacticAge(31);
     expect(myGalacticAge.marsAge).toEqual(31 * 1.88);
   });
 
-  test('should correctly create a mercuryAge property in GalacticAge class', () => {
+  test('should correctly create a jupiterAge property in GalacticAge class', () => {
     const myGalacticAge = new GalacticAge(31);
     expect(myGalacticAge.jupiterAge).toEqual(31 * 11.86);
   });
