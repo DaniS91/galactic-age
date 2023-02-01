b3eb7095caac1069a0ac93f0fb884ae0b4f7399e
DaniS91 <114887061+DaniS91@users.noreply.github.com>
2023-02-01 14:02:30 -0800

    add toFixed method to handle excessive digits from age calcs

--- src/age-calc.js
+++ src/age-calc.js
@@ -1,23 +1,22 @@
 export default class GalacticAge {
   constructor(age, passedAge, futureAge) {
     this.earthAge = age;
-    this.mercuryAge = age * .24;
-    this.venusAge = age * .62;
-    this.marsAge = age * 1.88;
-    this.jupiterAge = age * 11.86;
-    this.sinceEarthAge = age - passedAge;
-    this.sinceMercuryAge = this.sinceEarthAge * .24;
-    this.sinceVenusAge = this.sinceEarthAge * .62;
-    this.sinceMarsAge = this.sinceEarthAge * 1.88;
-    this.sinceJupiterAge = this.sinceEarthAge * 11.86;
-    this.untilEarthAge = futureAge - age;
-    this.untilMercuryAge = this.untilEarthAge * .24;
-    this.untilVenusAge = this.untilEarthAge * .62;
-    this.untilMarsAge = this.untilEarthAge * 1.88;
-    this.untilJupiterAge = this.untilEarthAge * 11.86;
-
-    listAges() {
-      return `${this.earthAge} Earth years, ${this.mercuryAge} Mercury years, ${this.VenusAge} Venus years, ${this.marsAge} Mars years, and ${this.jupiterAge} Jupiter years`;
-    }
+    this.mercuryAge = (age * .24).toFixed(2);
+    this.venusAge = (age * .62).toFixed(2);
+    this.marsAge = (age * 1.88).toFixed(2);
+    this.jupiterAge = (age * 11.86).toFixed(2);
+    this.sinceEarthAge = (age - passedAge).toFixed(2);
+    this.sinceMercuryAge = (this.sinceEarthAge * .24).toFixed(2);
+    this.sinceVenusAge = (this.sinceEarthAge * .62).toFixed(2);
+    this.sinceMarsAge = (this.sinceEarthAge * 1.88).toFixed(2);
+    this.sinceJupiterAge = (this.sinceEarthAge * 11.86).toFixed(2);
+    this.untilEarthAge = (futureAge - age).toFixed(2);
+    this.untilMercuryAge = (this.untilEarthAge * .24).toFixed(2);
+    this.untilVenusAge = (this.untilEarthAge * .62).toFixed(2);
+    this.untilMarsAge = (this.untilEarthAge * 1.88).toFixed(2);
+    this.untilJupiterAge = (this.untilEarthAge * 11.86).toFixed(2);
+  }
+  listAges() {
+    return `${this.earthAge} Earth years, ${this.mercuryAge} Mercury years, ${this.VenusAge} Venus years, ${this.marsAge} Mars years, and ${this.jupiterAge} Jupiter years`
   }
 }
